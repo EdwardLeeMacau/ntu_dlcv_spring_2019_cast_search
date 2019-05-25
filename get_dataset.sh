9113ee6552672ff1a2955471797c905b5c0c5784
Edward Lee <david1998zzz@gmail.com>
2019-05-25 13:41:30 +0800

    UPDATE get_dataset.sh

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,5 +1,21 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B6eKvaijfFUDQUUwd21EckhUbWs" -O $1 && rm -rf /tmp/cookies.txt
+# Download training set
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B6eKvaijfFUDQUUwd21EckhUbWs' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=0B6eKvaijfFUDQUUwd21EckhUbWs" -O trainset.zip && rm -rf /tmp/cookies.txt
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B6eKvaijfFUDd3dIRmpvSk8tLUk" -O $2 && rm -rf /tmp/cookies.txt
+# Download validation set
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B6eKvaijfFUDd3dIRmpvSk8tLUk' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=0B6eKvaijfFUDd3dIRmpvSk8tLUk" -O valset.zip && rm -rf /tmp/cookies.txt
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B6eKvaijfFUDbW4tdGpaYjgzZkU" -O $3 && rm -rf /tmp/cookies.txt
+# Download test set
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=0B6eKvaijfFUDbW4tdGpaYjgzZkU' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=0B6eKvaijfFUDbW4tdGpaYjgzZkU" -O testset.zip && rm -rf /tmp/cookies.txt
+
+# Make dataset directory
+mkdir ./WIDER
+
+# Unzip the downloaded zip file
+unzip ./trainset.zip -d ./WIDER/train
+unzip ./valset.zip -d ./WIDER/val
+unzip ./testset.zip -d ./WIDER/test
+
+# Remove the downloaded zip file
+rm ./trainset.zip
+rm ./valset.zip
+rm ./testset.zip
