f31b9e63129c7ed2090818682739ee6cbe9a8c05
Edward Lee <david1998zzz@gmail.com>
2019-06-13 03:02:26 +0800

    MODIFY imdb.py

--- imdb.py
+++ imdb.py
@@ -11,6 +11,8 @@
     |   |   |   |---<id>.jpg
     |   |   |---cast/
     |   |   |   |---<id>.jpg
+    |   |   |---cast.json
+    |   |   |---candidate.json
     |---|---<movie_name>/
     ...
 """
@@ -23,7 +25,7 @@ import random
 import time
 
 import numpy as np
-# import pandas as pd
+import pandas as pd
 import torch
 import torchvision.transforms as transforms
 from matplotlib import pyplot as plt
@@ -33,48 +35,88 @@ from torch.utils.data import DataLoader, Dataset
 
 import utils
 
-class IMDbDataset(Dataset):
+class IMDbTrainset(Dataset):
     def __init__(self, movie_path, feature_path, label_path, transform=None):
         assert ((movie_path is not None) or (feature_path is not None)), "movie_path or feature_path is needed for IMDbDataset"
         
         self.movie_path   = movie_path
         self.feature_path = feature_path
         self.label_path   = label_path
+        self.root_path    = os.path.dirname(self.movie_path)
 
         self.transform = transform
-        self.movies_name = os.listdir(movie_path)
-        with open(label_path, 'r') as jsonfile:
-            self.labels = None
+
+        self.movies         = os.listdir(self.movie_path)
+        self.candidate_json = [pd.read_json(os.path.join(self.movie_path, filename, 'candidate.json'), orient='index', typ='series') for filename in os.listdir(self.movie_path)]
+        self.cast_json      = [pd.read_json(os.path.join(self.movie_path, filename, 'cast.json'), orient='index', typ='series') for filename in os.listdir(self.movie_path)]
+
+        self.candidates = pd.concat(self.candidate_json, axis=0, keys=self.movies).reset_index()
+        self.casts      = pd.concat(self.cast_json, axis=0, keys=self.movies).sort_values().reset_index()
+        
+        # Total images in dataset
+        print("Total candidates in dataset: {}".format(self.candidates.shape))
+        # Without "others"
+        print("Total casts in dataset:      {}".format(self.casts.shape))
+        # print("Total casts in unique: {}".format(self.casts.shape))
+
+        # print(self.candidates)
+        # print(self.casts)
 
     def __len__(self):
-        raise NotImplementedError
+        return self.candidates.shape[0]
 
     def __getitem__(self, index):
-        # Get Movie
-        movie_name = self.movies_name[index]
-
-        # Cast dict / Candidate dict
-        cast_name = os.path.join(self.movie_path, movie_name, 'cast.json')
-        candidate_name = os.path.join(self.movie_path, movie_name, 'candidates.json')
-        
-        label = None
+        # Get 1 image, label
+        candidate, cast = self.candidates.iat[index, 1], self.candidates.iat[index, 2]
+        # print(index, candidate, cast)
         
         # ---------------------------------------------------
         # To Read the images
         # ---------------------------------------------------
-        pass
+        image = Image.open(os.path.join(self.root_path, candidate))
 
         # ---------------------------------------------------
-        # Features Output dimension: (frames, feature_dim)
-        # Images Output dimension:   (frames, channel, height, width)
+        # Features Output dimension: (feature_dim)
+        # Images Output dimension:   (channel, height, width)
         # ---------------------------------------------------
         if self.transform:
             image = self.transform(image)
 
-        return image, label
+        return image, cast
+
+def collate_fn(batch):
+    """
+      To define a function that reads the video by batch.
+ 
+      Params:
+      - batch: 
+          In pytorch, dataloader generate batch of traindata by this way:
+            `self.collate_fn([self.dataset[i] for i in indices])`
+          
+          In here, batch means `[self.dataset[i] for i in indices]`
+          It's a list contains (datas, labels)
+
+      Return:
+      - batch: the input tensors in shape (batch, c, h, w)
+    """
+    # ---------------------------------
+    # batch[i][j]
+    #   the type of batch[i] is tuple
+    # 
+    #   i=(0, size) means the batchsize
+    #   j=(0, 1) means the data / label
+    # ---------------------------------
+    
+    images = torch.Tensor([x[0] for x in batch])
+    
+    labels = None
+    if batch[0][1] is not None: # If labels exists
+        labels = torch.cat([x[1].unsqueeze(0) for x in batch], dim=0)
+    
+    return images, labels
 
 def dataloader_unittest():
-    dataset = IMDbDataset("./IMDb/val", None, "./IMDb/val_GT.json", transform=transforms.Compose([
+    dataset = IMDbTrainset("./IMDb/val", None, "./IMDb/val_GT.json", transform=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ]))
@@ -90,7 +132,7 @@ def dataloader_unittest():
     pass
 
 def main():
-    pass
+    dataloader_unittest()
 
 if __name__ == "__main__":
     main()
