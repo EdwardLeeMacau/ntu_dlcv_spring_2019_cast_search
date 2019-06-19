f41b6eaa5bb6997d6dfd4177e8c9f665b51f8f3a
r07631025 <49395895+r07631025@users.noreply.github.com>
2019-06-19 23:22:02 +0800

    Add files via upload

---
+++ get_res50model.sh
@@ -0,0 +1,8 @@
+# Download dataset from Dropbox
+wget https://www.dropbox.com/s/6tv01ngob44ihsr/resnet50_ft_weight.zip
+
+# Unzip the downloaded zip file
+unzip ./resnet50_gt_weight.zip
+
+# Remove the downloaded zip file
+rm ./resnet50_gt_weight.zip
