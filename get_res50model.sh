5dca8131e5dea26c16dd29b8b965632c8fa56345
ugotsuyokunaru <36882238+ugotsuyokunaru@users.noreply.github.com>
2019-06-20 00:28:04 +0800

    fix sh bug

--- get_res50model.sh
+++ get_res50model.sh
@@ -1,12 +1,14 @@
 # Download dataset from Dropbox
-wget https://www.dropbox.com/s/6tv01ngob44ihsr/resnet50_ft_weight.zip
+if ! [ -f "pretrain/resnet50_ft_weight.pkl" ]; then
+    wget https://www.dropbox.com/s/6tv01ngob44ihsr/resnet50_ft_weight.zip
+fi
 
 # Unzip the downloaded zip file
-unzip ./resnet50_gt_weight.zip
+unzip ./resnet50_ft_weight.zip
 
 # Remove the downloaded zip file
-rm ./resnet50_gt_weight.zip
+rm ./resnet50_ft_weight.zip
 
 # move to target folder
 mkdir pretrain
-mv resnet50_gt_weight.pkl ./pretrain/
+mv resnet50_ft_weight.pkl ./pretrain/
