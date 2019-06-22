b20effdb5abd97fb4771bad7a01b1573694c8a98
ugotsuyokunaru <36882238+ugotsuyokunaru@users.noreply.github.com>
2019-06-22 16:41:26 +0800

    fix model.py

--- model_res50.py
+++ model_res50.py
@@ -1,79 +1,73 @@
 """
   FileName     [ model.py ]
-  PackageName  [ layumi/Person_reID_baseline_pytorch ]
+  PackageName  [ final ]
   Synopsis     [ Model class ]
-
-  Library:
-  - apex: A PyTorch Extension, Tools for easy mixed precision and distributed training in Pytorch
-          https://github.com/NVIDIA/apex
-  - yaml: A human-readable data-serialization language, and commonly used for configuration files.
-  - pretrainedmodels: 
-          Install: pip install pretrainedmodels
-
-  Pretrain network:
-  - PCB: Part-based Convolutional Baseline
-         https://arxiv.org/abs/1711.09349
-         Beyond Part Models: Person Retrieval with Refined Part Pooling (and a Strong Convolutional Baseline)
-  - DenseNet:
-  - NAS:
-  - ResNet: 
       
-   feature_extractor_face(VGGFace2 pretrained)---->> model name
-   feature_extractor_orugin(Imagenet pretrained)
-   classifier--------->>fc_output  dropout
-                           drop = 0 ->no dropout
+  1. Feature_extractor_face   (VGGFace2 pretrained) ---->> model name
+  2. Feature_extractor_origin (Imagenet pretrained)
+  3. Classifier --------->> fc_output  dropout
+                            drop = 0 ->no dropout
 """
-
+import pickle
 import torch
 import torch.nn as nn
 from torch.autograd import Variable
+import torchvision
 
-class feature_extractor_face(nn.Module):
-    def __init__(self,model = 'pretrain/resnet50_ft_weight.pkl'):
-        super(feature_extractor_face, self).__init__()
-        import torchvision
-        import pickle
+class Feature_extractor_face(nn.Module):
+    def __init__(self, model='pretrain/resnet50_ft_weight.pkl'):
+        super(Feature_extractor_face, self).__init__()
         
-        resnet = torchvision.models.resnet50(num_classes=8631,pretrained = False)
+        # load pytorch resnet50
+        resnet = torchvision.models.resnet50(num_classes=8631, pretrained=False)
+
+        # fix layers
         for param in resnet.parameters():
             param.requires_grad = False
+        
+        # load pretrained weights
         with open(model, 'rb') as f:
             obj = f.read()
         weights = {key: torch.from_numpy(arr) for key, arr in pickle.loads(obj, encoding='latin1').items()}
-
         resnet.load_state_dict(weights, strict=False)
+
+        # change output dim (from 8631 to 2048)
         fc_features = resnet.fc.in_features
         resnet.fc = nn.Linear(fc_features, 2048)
         
-        self.resnet_layer = nn.Sequential(resnet,
-                            )
+        # defined all resnet layers
+        self.resnet_layer = resnet
 
     def forward(self, input_data):        
         feature = self.resnet_layer(input_data)
         return feature
 
-class feature_extractor_origin(nn.Module):
+class Feature_extractor_origin(nn.Module):
     def __init__(self):
-        super(feature_extractor_origin, self).__init__()
-        import torchvision
+        super(Feature_extractor_origin, self).__init__()
+
+        # load pytorch pretrained resnet50
         resnet = torchvision.models.resnet50(pretrained = True)
+
+        # fix layers
         for param in resnet.parameters():
             param.requires_grad = False
+
+        # change output dim (from 8631 to 2048)
         fc_features = resnet.fc.in_features
         resnet.fc = nn.Linear(fc_features, 2048)
-        self.resnet_layer = nn.Sequential(resnet,
-                            )
+
+        # defined all resnet layers
+        self.resnet_layer = resnet
 
     def forward(self, input_data):        
         feature = self.resnet_layer(input_data)
         return feature
 
-
-
-
-class classifier(nn.Module):
+class Classifier(nn.Module):
     def __init__(self,fc_out = 1024,drop = 0.5):
-        super(classifier, self).__init__()
+        super(Classifier, self).__init__()
+
         self.resnet_classifier = nn.Sequential(
                             nn.BatchNorm1d(2048),
                             nn.ReLU(),
@@ -87,15 +81,18 @@ class classifier(nn.Module):
 
 def model_structure_unittest():
     """ Debug model structure """
-    
+    # img
     imgs = Variable(torch.FloatTensor(8, 3, 224, 224))
-    res = feature_extractor_face()
-    res_last = classifier()
+
+    # model
+    res = Feature_extractor_face()
+    res_last = Classifier()
+
+    # infernece
     output = res(imgs)
 
     feature = res_last(output)
     print(feature.size())
-
 
 if __name__ == "__main__":
     model_structure_unittest()
